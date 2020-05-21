a1f72f6dc0b17d6236ceffe33a5ee840758bc301
Symbol <Symbolistic@live.com>
2020-05-21 15:21:02 -0400

    almost done

--- src/DrumApp.js
+++ src/DrumApp.js
@@ -1,117 +1,123 @@
 import React from 'react'
 
+const keys = [
+    {
+        key: 'Q',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+        ref: React.createRef(),
+        display: "Heater 1"
+    },
+
+    {
+        key: 'W',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+        ref: React.createRef(),
+        display: "Heater 2"
+    },
+
+    {
+        key: 'E',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+        ref: React.createRef(),
+        display: "Heater 3"
+    },
+
+    {
+        key: 'A',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+        ref: React.createRef(),
+        display: "Heater 4"
+    },
+
+    {
+        key: 'S',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+        ref: React.createRef(),
+        display: "Clap"
+    },
+
+    {
+        key: 'D',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+        ref: React.createRef(),
+        display: "Open HH"
+    },
+
+    {
+        key: 'Z',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+        ref: React.createRef(),
+        display: "Kick n' Hat"
+    },
+
+    {
+        key: 'X',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+        ref: React.createRef(),
+        display: "Kick"
+    },
+
+    {
+        key: 'C',
+        src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+        ref: React.createRef(),
+        display: "CLosed HH"
+    },
+]
+
 class DrumApp extends React.Component {
     constructor () {
         super()
         this.state = {
-
-        }
-        this.audioRefQ = React.createRef()
-        this.audioRefW = React.createRef()
-        this.audioRefE = React.createRef()
-        this.audioRefA = React.createRef()
-        this.audioRefS = React.createRef()
-        this.audioRefD = React.createRef()
-        this.audioRefZ = React.createRef()
-        this.audioRefX = React.createRef()
-        this.audioRefC = React.createRef()
+            keys,
+            display: '',
+            color: "blue"
+        };
+        this.mainInput = React.createRef();
         this.handleClick = this.handleClick.bind(this);
+        this.handleKey = this.handleKey.bind(this);
     }
 
-    handleClick (event) {
-        this.audioRefA.current.currentTime = 0;
-        this.audioRefA.current.play();
+    handleClick (key) {
+        console.log(key)
+        const audio = this.state.keys.find(x => x.key === key);
+
+        audio.ref.current.currentTime = 0;
+        audio.ref.current.play();
+
+        this.setState({display: audio.display})
     }
+
+    handleKey (event) {
+        console.log(event.target)
+
+        const audio = this.state.keys.find(x => x.key === event.key.toUpperCase());
+        if(audio){
+            audio.ref.current.currentTime = 0;
+            audio.ref.current.play();
+
+            this.setState({display:audio.display})
+        }
+    }
+
+    componentDidMount () {
+        this.mainInput.current.focus();
+    }
+
 
     render() {
         return (
-            <div id='DrumAppContainer'>
+            <div id='DrumAppContainer' tabIndex='0' onKeyPress={this.handleKey} ref={this.mainInput}>
                 <div id="side1">
-                    <div className="drum-pad" onClick={this.handleClick}>Q
-                        <audio 
-                            id='Q' 
-                            className="clip" 
-                            src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
-                            ref={this.audioRefQ}
-                        />
-                    </div>
-
-                    <div className="drum-pad">W
-                        <audio 
-                            id='W' 
-                            className="clip" 
-                            src='https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-                            ref={this.audioRefW}
-                        />
-                    </div>
-
-                    <div className="drum-pad">E
-                        <audio 
-                            id='E' 
-                            className="clip" 
-                            src='https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-                            ref={this.audioRefE}
-                        />
-                    </div>
-
-                    <div className="drum-pad">A
-                        <audio 
-                            id='A' 
-                            className="clip" 
-                            src='https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
-                            ref={this.audioRefA}
-                        />
-                    </div>
-
-                    <div className="drum-pad">S
-                        <audio 
-                            id='S' 
-                            className="clip" 
-                            src='https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-                            ref={this.audioRefS}
-                        />
-                    </div>
-
-                    <div className="drum-pad">D
-                        <audio 
-                            id='D' 
-                            className="clip" 
-                            src='https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-                            ref={this.audioRefD}
-                        />
-                    </div>
-
-                    <div className="drum-pad">Z
-                        <audio 
-                            id='Z' 
-                            className="clip" 
-                            src='https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-                            ref={this.audioRefZ}
-                        />
-                    </div>
-
-                    <div className="drum-pad">X
-                        <audio 
-                            id='X' 
-                            className="clip" 
-                            src='https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-                            ref={this.audioRefX}
-                        />
-                    </div>
-
-                    <div className="drum-pad">C
-                        <audio 
-                            id='C' 
-                            className="clip"   
-                            src='https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-                            ref={this.audioRefC}
-                        />
-                    </div>
-
+                    {this.state.keys.map(({key, src, ref}) => (
+                        <button key={key} className="drum-pad" onClick={() => this.handleClick(key)} style={{"color":this.state.color}}>{key}
+                            <audio id={key} className="clip" src={src} ref={ref}></audio>
+                        </button>
+                    ))}
                 </div>
 
                 <div id="side2">
-                    <div id="display"><h1>Output Here</h1></div>
+                    <div id="display"><h1>{this.state.display}</h1></div>
                 </div>
             </div>
         )
